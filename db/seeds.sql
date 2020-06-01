8a36fd9e4ad7ccedcbc99800dfb65bbc9427f07f
Andrew Kessler <andrew.kessler95@gmail.com>
2020-05-31 20:58:32 -0600

    sql and seeds set up, added gitignore, installed dependancies / init

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db (burger_name) VALUES ('Bacon Burger')
+INSERT INTO burgers_db (burger_name) VALUES ('Mushroom Burger')
+INSERT INTO burgers_db (burger_name, devoured) VALUES ('Veggie Burger', true)
